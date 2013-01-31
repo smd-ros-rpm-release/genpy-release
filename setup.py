9f6f6109dae7e2e32b31759d3e4fd889eef6a4d2
Dirk Thomas <dthomas@willowgarage.com>
2013-01-30 17:47:24 -0800

    Rebase from 'upstream' @ version '0.3.7'

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
+import sys
+from xml.etree.ElementTree import ElementTree
 
-d = generate_distutils_setup(
-    packages=['genpy'],
-    package_dir={'': 'src'},
-    requires=['genmsg']
-)
+try:
+    root = ElementTree(None, 'stack.xml')
+    version = root.findtext('version')
+except Exception, e:
+    print >> sys.stderr, 'Could not extract version from your stack.xml:\n%s' % e
+    sys.exit(-1)
 
-setup(**d)
+sys.path.insert(0, 'src')
+
+setup(name = 'genpy',
+      version = version,
+      packages = ['genpy'],
+      package_dir = {'': 'src'},
+      install_requires = ['genmsg'],
+      scripts = ['scripts/genmsg_py.py', 'scripts/gensrv_py.py'],
+      author = "Ken Conley",
+      author_email = "kwc@willowgarage.com",
+      url = "http://www.ros.org/wiki/genpy",
+      download_url = "http://pr.willowgarage.com/downloads/genpy/",
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License" ],
+      description = "ROS msg/srv Python generation",
+      long_description = """\
+Library and scripts for generating ROS message data structures in Python.
+""",
+      license = "BSD"
+      )
